d827d6701aa6d4c460f571854b06b7478a523c34
AlHassanEdris <hassandris89@gmail.com>
2026-04-30 19:18:23 +0300

    Create model.py

---
+++ models/vision/model.py
@@ -0,0 +1,9 @@
+# Image Model Placeholder
+
+def load_image_model():
+    print("Loading image model...")
+    return None
+
+def predict_image(image):
+    print("Analyzing image...")
+    return 0.5
